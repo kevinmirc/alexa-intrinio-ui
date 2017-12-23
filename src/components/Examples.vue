cc7ef12906b6ca05be087eec637392da3e1c5bfd
Kevin Mircovich <kmircovich1@gmail.com>
2017-12-22 23:37:42 -0500

    init commit

---
+++ src/components/Examples.vue
@@ -0,0 +1,79 @@
+<template lang="html">
+  <Row type="flex" class="section" justify="center">
+    <Col>
+      <h1 class="title">What you can ask Alexa:</h1>
+      <p class="sub-title-2">
+        Ask for any <span class="data-point">Data Point</span> for any publicly traded <span class="company-name">Company</span>
+      </p>
+      <Row type="flex" align="middle" justify="center" style="padding: 40px;">
+        <Card class="card">
+          <Icon type="quote"></Icon>
+          <p class="example">
+            "What was the <span class="data-point">opening price</span> for <span class="company-name">Apple</span>?"
+          </p>
+        </Card>
+        <Card class="card">
+          <Icon type="quote"></Icon>
+          <p class="example">
+            "What was the <span class="data-point">closing price</span> for <span class="company-name">Amazon</span>?"
+          </p>
+        </Card>
+        <Card class="card">
+          <Icon type="quote"></Icon>
+          <p class="example">
+            "What was the <span class="data-point">last price</span> for <span class="company-name">Facebook</span>?"
+          </p>
+        </Card>
+        <Card class="card">
+          <Icon type="quote"></Icon>
+          <p class="example">
+            "What is the <span class="data-point">52 week low</span> for <span class="company-name">Bank of America</span>"
+          </p>
+        </Card>
+        <Card class="card">
+          <Icon type="quote"></Icon>
+          <p class="example">
+            "How many <span class="data-point">employees</span> does <span class="company-name">Tesla</span> have"
+          </p>
+        </Card>
+      </Row>
+    </Col>
+  </Row>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.section {
+  padding: 16px;
+}
+.title {
+    font-size: 24px;
+    padding-top: 40px;
+}
+.sub-title-2 {
+  font-size: 20px;
+}
+span.data-point {
+  font-weight: 600;
+  color: #2fc17f;
+}
+
+span.company-name {
+  font-weight: 600;
+  color: #15abe1;
+}
+
+.example {
+  font-size: 18px;
+  color: #2c3e50;
+  text-align: center;
+}
+.card {
+  width: 300px;
+  margin: 8px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="">
+
+    <Affix>
+      <Row type="flex" align="middle" justify="space-around" class="background-dark">
+        <Col span="12">
+          <h1 style="font-size: 20px;">Market Savvy Alexa Skill</h1>
+        </Col>
+        <Col span="12">
+          <div class="cta">
+            <a href="https://www.amazon.com/dp/B01N4OW2Z4/ref=syps" target="_blank">
+              <Button type="primary" style="background: #35d68d; border-color: #35d68d">Get the Alexa Skill</Button>
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Affix>
+
+    <Examples></Examples>
+    <DataPoints></DataPoints>
+  </div>
+</template>
+
+<script>
+import Examples from './Examples'
+import DataPoints from './DataPoints'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return { alexaSkillLink: 'https://www.amazon.com/dp/B01N4OW2Z4/ref=syps' }
+  },
+  components: { Examples, DataPoints }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p.sub-header {
+  font-size: 16px;
+}
+.cta {
+  margin: 16px 16px;
+  font-weight: 600;
+}
+.background-dark {
+  background: rgba(0, 0, 0, 0.8);
+  color: #fff;
+}
+</style>
